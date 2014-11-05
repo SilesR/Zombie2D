13b2a0a1136ecd4ff54515efc927534f7fd0da81
SilesR <rafasiles@gmail.com>
2014-11-05 18:33:10 +0100

    Tarea01 Movimiento Básico
    Movimiento horizontal del personaje, añadido cicle collider en pies para
    el personaje.

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(2,3);
+	//piravete Animator animator;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+				var absVelX = Mathf.Abs (rigidbody2D.velocity.x); //velocidad de eje x absoluta = a la velocidad que tenga el rigidbody
+
+				var forceX = 0f;
+				var forceY = 0f;
+	
+				//Calculate Veloity X
+				if (Input.GetKey ("right")) {
+						if (absVelX < maxVelocity.x)
+								forceX = speed;
+						this.transform.localScale = new Vector3 (1, 1, 1);
+				} else if (Input.GetKey ("left")) {
+						if (absVelX < maxVelocity.x)
+								forceX = -speed;
+						this.transform.localScale = new Vector3 (-1, 1, 1);
+				}
+				rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+		}
+	
+}
