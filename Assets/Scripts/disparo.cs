541b182f562cbfed06fc2b15c3d02b7937664628
SilesR <rafasiles@gmail.com>
2014-11-10 18:44:43 +0100

    Fallos en Animator. [PENDIENTE]
    El script da problemas con las animaciones de disparo y movimiento.
    Script comentados para que no de fallos, pero faltan por tanto las
    animaciones.

---
+++ Assets/Scripts/disparo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disparo : MonoBehaviour {
+
+
+	public GameObject bala;
+	public Transform puntoDeDisparo;
+
+	public void Fire(){
+		if (bala != null) {
+
+			var clone = Instantiate (bala, puntoDeDisparo.position,Quaternion.identity)as GameObject;
+			clone.transform.localScale=transform.localScale;
+
+		}
+
+}
+}
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -5,9 +5,14 @@ public class playerScript : MonoBehaviour {
 
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(2,3);
-	//piravete Animator animator;
+	private Animator animacion;
 
+	void Star(){
+
+				//animator = GetComponent<Animator>();
+		animacion = GetComponent<Animator>();
 	
+		}
 	// Update is called once per frame
 	void Update () {
 
@@ -17,16 +22,34 @@ public class playerScript : MonoBehaviour {
 				var forceY = 0f;
 	
 				//Calculate Veloity X
-				if (Input.GetKey ("right")) {
-						if (absVelX < maxVelocity.x)
-								forceX = speed;
-						this.transform.localScale = new Vector3 (1, 1, 1);
-				} else if (Input.GetKey ("left")) {
-						if (absVelX < maxVelocity.x)
-								forceX = -speed;
-						this.transform.localScale = new Vector3 (-1, 1, 1);
+			if (Input.GetKey ("right")) {
+				if(rigidbody2D.velocity.x <0){
+					rigidbody2D.velocity= new Vector2 (0, rigidbody2D.velocity.y);//Si voy a la izquierda y cambio a la derecha freno.
 				}
-				rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+				if (absVelX < maxVelocity.x)
+					forceX = speed;
+				this.transform.localScale = new Vector3 (1, 1, 1);
+			} else if (Input.GetKey ("left")) {
+				if(rigidbody2D.velocity.x >0){
+					rigidbody2D.velocity= new Vector2 (0, rigidbody2D.velocity.y);//Si voy a la derecha y cambio a la izquierda freno.
+				}
+				if (absVelX < maxVelocity.x)
+					forceX = -speed;
+				this.transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		//Comparamos la velocidad actual para ssaber si es 0.
+		if (absVelX > 0) {
+			//animacion.SetFloat ("velocity", absVelX);
+		}
+		rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+
+		if (Input.GetKey ("c")) {
+			//animacion.SetBool ("fire", true);
+		} else {
+			//animacion.SetBool ("fire", false);
+
+		}
+
 		}
 	
 }
