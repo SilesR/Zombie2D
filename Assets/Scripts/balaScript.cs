b76376b8b8c9e0b16857a4143d8ccf8cfb91d6bc
SilesR <rafasiles@gmail.com>
2014-11-10 18:53:07 +0100

    disparo bala[PENDIENTE]

---
+++ Assets/Scripts/balaScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balaScript : MonoBehaviour {
+	public Vector2 velocity = new Vector2(5,0);
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = velocity * transform.localScale.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
