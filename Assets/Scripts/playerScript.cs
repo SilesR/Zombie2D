16c9024118fafd3c719c27c1582c284d08b017fb
SilesR <rafasiles@gmail.com>
2014-11-12 16:39:17 +0100

    Corrección Script PLAYER

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -5,51 +5,54 @@ public class playerScript : MonoBehaviour {
 
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(2,3);
-	private Animator animacion;
-
-	void Star(){
-
-				//animator = GetComponent<Animator>();
-		animacion = GetComponent<Animator>();
+	public Light linterna;
+	private Animator animator;
 	
-		}
+	// Use this for initialization
+	void Start () {
+		
+		animator = GetComponent<Animator> ();
+		
+		
+	}
+	
 	// Update is called once per frame
 	void Update () {
-
-				var absVelX = Mathf.Abs (rigidbody2D.velocity.x); //velocidad de eje x absoluta = a la velocidad que tenga el rigidbody
-
-				var forceX = 0f;
-				var forceY = 0f;
-	
-				//Calculate Veloity X
-			if (Input.GetKey ("right")) {
-				if(rigidbody2D.velocity.x <0){
-					rigidbody2D.velocity= new Vector2 (0, rigidbody2D.velocity.y);//Si voy a la izquierda y cambio a la derecha freno.
-				}
-				if (absVelX < maxVelocity.x)
-					forceX = speed;
-				this.transform.localScale = new Vector3 (1, 1, 1);
-			} else if (Input.GetKey ("left")) {
-				if(rigidbody2D.velocity.x >0){
-					rigidbody2D.velocity= new Vector2 (0, rigidbody2D.velocity.y);//Si voy a la derecha y cambio a la izquierda freno.
-				}
-				if (absVelX < maxVelocity.x)
-					forceX = -speed;
-				this.transform.localScale = new Vector3 (-1, 1, 1);
+		
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		var forceX = 0f;
+		var forceY = 0f;
+		
+		// Calculate Velocity X
+		if (Input.GetKey("right"))
+		{
+			if(rigidbody2D.velocity.x < 0){
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			}
+			if(absVelX < maxVelocity.x)
+				forceX = speed;
+			this.transform.localScale = new Vector3(1, 1, 1);
+			//linterna.transform.rotation = Quaternion.LookRotation(new Vector3(90,0,0));
 		}
-		//Comparamos la velocidad actual para ssaber si es 0.
-		if (absVelX > 0) {
-			//animacion.SetFloat ("velocity", absVelX);
+		else if (Input.GetKey("left"))
+		{
+			if(rigidbody2D.velocity.x > 0)
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			if (absVelX < maxVelocity.x)
+				forceX = -speed;
+			this.transform.localScale = new Vector3(-1, 1, 1);
+			//linterna.transform.rotation = Quaternion.LookRotation(new Vector3(-90,0,0));
 		}
-		rigidbody2D.AddForce (new Vector2 (forceX, forceY));
-
+		if(absVelX > 0)
+			animator.SetFloat("Velocity",absVelX);
+		rigidbody2D.AddForce(new Vector2(forceX, forceY));
+		
 		if (Input.GetKey ("c")) {
-			//animacion.SetBool ("fire", true);
-		} else {
-			//animacion.SetBool ("fire", false);
-
+			animator.SetBool ("Fire", true);
+			
+		}else{
+			animator.SetBool ("Fire", false);
 		}
+	}
 
-		}
-	
 }
