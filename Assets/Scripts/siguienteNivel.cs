a11b04c8e379502e55c9272dd55beb6cf61b1216
SilesR <rafasiles@gmail.com>
2014-12-03 18:54:58 +0100

    Siguiente nivel por tiempo

---
+++ Assets/Scripts/siguienteNivel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siguienteNivel : MonoBehaviour {
+
+	public string nombreNivel;
+	bool cargando=false;
+	public float tiempoEspera=3f;
+
+
+
+	void OnTriggerStay2D(Collider2D target){
+				if (target.transform.tag == "Player") {
+						if (!cargando) {
+								StartCoroutine (cargarNivel ());
+						}
+				}
+		}
+
+	void OnTriggerExit2D(Collider2D target){
+		cargando = false;
+		}
+
+	IEnumerator cargarNivel(){
+		cargando = true;
+		yield return new WaitForSeconds(tiempoEspera);
+		if(cargando){
+			Application.LoadLevel(nombreNivel);
+
+		}
+}
+}
