e085082fe0a5f76b780fbc8fd31495d4076c7cf8
SilesR <rafasiles@gmail.com>
2014-12-04 19:36:36 +0100

    Menu

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+	public string nombreNivel;
+
+	public void salir(){
+				Application.Quit ();
+		}
+	public void jugar(){
+		Application.LoadLevel (nombreNivel);
+		}
+}
